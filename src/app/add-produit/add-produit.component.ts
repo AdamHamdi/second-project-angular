814134ab40d4467c0f2ccd59448e3354002cc078
Adam Hamdi <prodev.webdesign@gmail.com>
2021-02-23 22:36:54 +0100

    data binding

--- src/app/add-produit/add-produit.component.ts
+++ src/app/add-produit/add-produit.component.ts
@@ -3,11 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-add-produit',
   templateUrl: './add-produit.component.html',
-  
+
 })
 export class AddProduitComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+   
+   }
 
   ngOnInit(): void {
   }
--- src/app/produits/produits.component.ts
+++ src/app/produits/produits.component.ts
@@ -3,11 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-produits',
   templateUrl: './produits.component.html',
-  
+
 })
 export class ProduitsComponent implements OnInit {
-
-  constructor() { }
+  //Model
+    //data binding1
+  produits: string[];
+    constructor() {
+      //data binding2
+      this.produits=["PC Asus","Imprimante Epson","Tablette Samsung"];
+     }
 
   ngOnInit(): void {
   }
