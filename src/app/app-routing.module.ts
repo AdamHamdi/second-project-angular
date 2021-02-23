d1b9eb11103a18a73295cd160bcccd5d0030acd7
Adam Hamdi <prodev.webdesign@gmail.com>
2021-02-23 19:48:12 +0100

    third  commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {ProduitsComponent} from './produits/produits.component';
+import { AddProduitComponent } from './add-produit/add-produit.component';
 
 const routes: Routes = [
   {path:"produits",component:ProduitsComponent},
+  {path:"add-produit",component:AddProduitComponent},
 ];
 
 @NgModule({
