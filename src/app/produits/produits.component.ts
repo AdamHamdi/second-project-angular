bda4cf8744f6566b5f43d6ae126ceb64c545d0bc
Adam Hamdi <prodev.webdesign@gmail.com>
2021-02-24 17:18:21 +0100

    creation du modèle

--- src/app/produits/produits.component.ts
+++ src/app/produits/produits.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Produit } from '../model/produit.model';
+import { Produit } from './../model/produit.model';
 
 @Component({
   selector: 'app-produits',
@@ -11,7 +11,12 @@ export class ProduitsComponent implements OnInit {
   produits: Produit[];
     constructor() {
       //data binding2
-    //   this.produits=["PC Asus","Imprimante Epson","Tablette Samsung"];
+       this.produits=[
+         //notation json
+         {idProduit:1, nomProduit:"PC Asus",prixProduit:3000.600,dateCreation:new Date("01/14/2010")},
+         {idProduit:2, nomProduit:"Imprimante Epson",prixProduit:450, dateCreation:new Date("12/17/2010")},
+         {idProduit:3, nomProduit:"Tablette Samsung",prixProduit:900.123,dateCreation:new Date("02/20/2010")},
+       ];
     }
 
   ngOnInit(): void {
