550157967a0d15025e00435be244273d39fd2776
Adam Hamdi <prodev.webdesign@gmail.com>
2021-03-01 19:32:38 +0100

    login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,19 @@ import { RouterModule, Routes } from '@angular/router';
 import {ProduitsComponent} from './produits/produits.component';
 import { AddProduitComponent } from './add-produit/add-produit.component';
 import { UpdateProduitComponent } from './update-produit/update-produit.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {path:"produits",component:ProduitsComponent},
   {path:"add-produit",component:AddProduitComponent},
   {path:"",redirectTo:"produits", pathMatch:"full"},
-  {path: "updateProduit/:id", component: UpdateProduitComponent}
+  {path: "updateProduit/:id", component: UpdateProduitComponent},
+  {path: 'login', component:LoginComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
+
 
 exports: [RouterModule]
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppComponent } from './app.component';
 import { ProduitsComponent } from './produits/produits.component';
 import { AddProduitComponent } from './add-produit/add-produit.component';
 import { UpdateProduitComponent } from './update-produit/update-produit.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProduitsComponent,
     AddProduitComponent,
-    UpdateProduitComponent
+    UpdateProduitComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
